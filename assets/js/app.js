b53cb349b9461a5640ba6427055d066b995e0a73
Lucas Perez <lucastperez@gmail.com>
2024-12-13 18:29:50 -0300

    Add sortablejs

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,18 +14,23 @@
 //
 //     import "some-package"
 //
-
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import "phoenix_html"
 // Establish Phoenix Socket and LiveView configuration.
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
+import SortableCounters from "./sortable_counter_config"
+
+let Hooks = {
+  SortableCounters: SortableCounters
+}
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {
   longPollFallbackMs: 2500,
-  params: {_csrf_token: csrfToken}
+  params: {_csrf_token: csrfToken},
+  hooks: Hooks
 })
 
 // Show progress bar on live navigation and form submits
---
+++ assets/js/sortable_counter_config.js
@@ -0,0 +1,19 @@
+import Sortable from "../vendor/sortable"
+
+SortableCounters = {
+  mounted() {
+    Sortable.create(this.el, {
+      animation: 150,
+      swapThreshold: 1,
+      direction: 'vertical',
+      onEnd: (event) => {
+        this.pushEvent("reorder_counters", {
+          oldIndex: event.oldIndex,
+          newIndex: event.newIndex,
+        });
+      },
+    });
+  }
+};
+
+export default SortableCounters;
